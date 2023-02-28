1d4b804477ed39e0b49bff27f57e1905a04f55cc
m.backi <backimichal@gmail.com>
2023-02-28 15:46:59 +0100

    poprawka1

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,37 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+import SimpleLightbox from "./node_modules/dist/simple-lightbox.js";
+import "./node_modules/dist/simplelightbox/dist/simple-lightbox.min.css";
+const galleryContainer = document.querySelector(".gallery");
+
+const createGalleryItem = (item) => {
+  const galleryItem = document.createElement("div");
+  galleryItem.classList.add("gallery__item");
+
+  const link = document.createElement("a");
+  link.classList.add("gallery__link");
+  link.href = item.original;
+
+  const image = document.createElement("img");
+  image.classList.add("gallery__image");
+  image.src = item.preview;
+  image.alt = item.description;
+
+  link.appendChild(image);
+  galleryItem.appendChild(link);
+
+  return galleryItem;
+};
+
+const galleryItemsElements = galleryItems.map((item) =>
+  createGalleryItem(item)
+);
+galleryContainer.append(...galleryItemsElements);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captions: true, // włączenie podpisów
+  captionDelay: 250, // opóźnienie pojawiania się podpisu
+  captionsData: "alt", // wykorzystanie atrybutu alt jako treści podpisu
+});
 
 console.log(galleryItems);
