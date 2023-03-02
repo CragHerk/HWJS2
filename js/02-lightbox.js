eae1f8a040e94cc516d958f41c4113065675cac3
m.backi <backimichal@gmail.com>
2023-03-02 12:58:30 +0100

    działa wszystko

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,6 +1,5 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
 const galleryContainer = document.querySelector(".gallery");
 
 const createGalleryItem = (item) => {
@@ -9,10 +8,13 @@ const createGalleryItem = (item) => {
 
   const link = document.createElement("a");
   link.classList.add("gallery__link");
-  link.href = "#";
+  link.href = item.original;
+  link.setAttribute("data-lightbox", "gallery");
+  link.classList.add("lightbox");
 
   const image = document.createElement("img");
   image.classList.add("gallery__image");
+  image.classList.add("img-fluid");
   image.src = item.preview;
   image.dataset.source = item.original;
   image.alt = item.description;
@@ -26,5 +28,22 @@ const createGalleryItem = (item) => {
 const renderGallery = (items) => {
   const galleryItems = items.map((item) => createGalleryItem(item));
   galleryContainer.append(...galleryItems);
+
+  const lightbox = new SimpleLightbox(".lightbox", {
+    captions: true,
+    captionsData: "alt",
+    captionDelay: 250,
+    close: true,
+    closeText: "×",
+    nav: true,
+    scrollZoom: false,
+    history: false,
+    keyboard: true,
+  });
+  galleryContainer.addEventListener("click", (event) => {
+    event.preventDefault();
+    lightbox.open();
+  });
 };
+console.log(galleryItems);
 renderGallery(galleryItems);
